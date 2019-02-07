9d2397e717449b755a695b130d257ab7055b000b
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-02-07 15:27:58 -0500

    fixed media query, aligned most content, still need to fix width issues

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function showSlides() {
   }
   
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += "active";
